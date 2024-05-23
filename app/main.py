8badb80bae00a542a6f8801fb04bf2718662e6b6
Wouter Mostard <wouter.mostard@gmail.com>
2024-05-24 00:31:02 +0200

    add connection to Azure SMB

--- app/main.py
+++ app/main.py
@@ -2,15 +2,32 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 import pydeck as pdk
+import dotenv
+import os
+from azure.storage.fileshare import ShareFileClient
 
 from constants import COLOR_MAPPING
 
-
 DATA_FOLDER: Path = Path('./data/gtfs')
+
+
+dotenv.load_dotenv()
+
+conn_string = os.getenv('connection_string')
+
+def load_data(file_name: str):
+    if file_name not in os.listdir(DATA_FOLDER):
+        service = ShareFileClient.from_connection_string(conn_str=conn_string, share_name='gtfs', file_path=file_name)
+
+        data = service.download_file()
+        data.readinto(open(DATA_FOLDER / file_name, 'wb'))
+
+    return pd.read_csv(DATA_FOLDER / file_name)
+
 
 @st.cache_data
 def load_path_data():
-    data = pd.read_csv(DATA_FOLDER / 'shapes.txt')
+    data = load_data('shapes.txt')
     data = data[data.shape_pt_sequence % 20 == 0]
     data.columns = ['shape_id', 'shape_seq', 'lat', 'lon', 'dist_traveled']
     data.set_index('shape_id', inplace=True)
@@ -30,16 +47,16 @@ def load_path_data():
 
 @st.cache_data
 def load_path_to_agency_data():
-    unique_shape_trips = pd.read_csv(DATA_FOLDER / 'trips.txt').drop_duplicates(subset='shape_id').set_index('shape_id')[['route_id']]
-    unique_shapes = pd.read_csv(DATA_FOLDER / 'shapes.txt').drop_duplicates(subset='shape_id')[['shape_id']].set_index('shape_id')
+    unique_shape_trips = load_data('trips.txt').drop_duplicates(subset='shape_id').set_index('shape_id')[['route_id']]
+    unique_shapes = load_data('shapes.txt').drop_duplicates(subset='shape_id')[['shape_id']].set_index('shape_id')
 
     shape_routes = unique_shapes.join(unique_shape_trips, how='left')[['route_id']].reset_index().set_index('route_id')
 
-    routes = pd.read_csv(DATA_FOLDER /'routes.txt').drop_duplicates('route_id').set_index('route_id')
+    routes = load_data('routes.txt').drop_duplicates('route_id').set_index('route_id')
 
     shape_agency = shape_routes.join(routes)[['shape_id', 'agency_id']].reset_index().drop('route_id', axis=1)
 
-    agency = pd.read_csv(DATA_FOLDER / 'agency.txt').set_index('agency_id')
+    agency = load_data('agency.txt').set_index('agency_id')
 
     agency_data = shape_agency.join(agency, on='agency_id')[['shape_id', 'agency_id', 'agency_name']].set_index('shape_id')
     agency_data['color'] = [COLOR_MAPPING[idx] for idx in agency_data.agency_id.values]
